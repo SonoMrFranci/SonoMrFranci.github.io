37fd18b8620429d28a847394a2e1bb1c36c7b2b6
yukinoir_ <115485381+SonoMrFranci@users.noreply.github.com>
2024-03-02 19:42:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let scoreTeam1 = 0;
+let scoreTeam2 = 0;
+
+function incrementScore(team) {
+    if (team === 1) {
+        scoreTeam1++;
+        document.getElementById('score1').innerText = scoreTeam1;
+    } else if (team === 2) {
+        scoreTeam2++;
+        document.getElementById('score2').innerText = scoreTeam2;
+    }
+}
+
+function resetScores() {
+    scoreTeam1 = 0;
+    scoreTeam2 = 0;
+    document.getElementById('score1').innerText = scoreTeam1;
+    document.getElementById('score2').innerText = scoreTeam2;
+}
