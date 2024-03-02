b8050fb07fe32a28aa422f1df96a90bef4761450
yukinoir_ <115485381+SonoMrFranci@users.noreply.github.com>
2024-03-02 21:30:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ function incrementScore(team) {
     }
 }
 
+function decrementScore(team) {
+    if (team === 1) {
+        scoreTeam1--;
+        document.getElementById('score1').innerText = scoreTeam1;
+    } else if (team === 2) {
+        scoreTeam2--;
+        document.getElementById('score2').innerText = scoreTeam2;
+    }
+}
+
 function resetScores() {
     scoreTeam1 = 0;
     scoreTeam2 = 0;
