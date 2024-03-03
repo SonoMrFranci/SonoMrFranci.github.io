2f6f4c7bafcda9a211e1cce87ce97b2a074b24ab
yukinoir_ <115485381+SonoMrFranci@users.noreply.github.com>
2024-03-03 21:52:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,25 @@ function showError(message) {
         errorElement.style.display = 'none';
     }, 3000);
 }
+
+
+function setTeamNames() {
+    var team1NameInput = document.getElementById("team1Name");
+    var team2NameInput = document.getElementById("team2Name");
+    var team1Label = document.getElementById("team1Label");
+    var team2Label = document.getElementById("team2Label");
+    var scoreboardContainer = document.querySelector(".scoreboard-container");
+
+    if (team1NameInput.value.trim() === "" || team2NameInput.value.trim() === "") {
+        document.getElementById("error-message").innerText = "Inserisci i nomi di entrambe le squadre.";
+        return;
+    }
+
+    team1Label.innerText = team1NameInput.value;
+    team2Label.innerText = team2NameInput.value;
+
+    document.querySelector(".team-inputs").style.display = "none";
+    document.getElementById("error-message").innerText = "";
+
+    scoreboardContainer.style.display = "flex";
+}
